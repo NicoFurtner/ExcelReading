d86e8de4b62d8c386e9a9a5823d424ca50c90360
Nico Furtner <nfurtner@student.tgm.ac.at>
2024-10-23 10:54:39 +0200

    Check File Method added & Tested in Main Class

--- src/main/java/org/example/ExcelReader.java
+++ src/main/java/org/example/ExcelReader.java
@@ -1,13 +1,16 @@
 package org.example;
 
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
  * Klasse um eine ExcelDatei zu lesen und evtl später zu verarbeiten und umzuwandeln
+ * Quelle für excelreaderstring methode:  * Quelle: https://www.geeksforgeeks.org/opening-existing-excel-sheet-in-java-using-apache-poi/
  * @author Nico Furtner
  * @version 2024-10-16
  */
@@ -32,6 +35,38 @@ public class ExcelReader {
             e.printStackTrace();
         }
         return text;
+    }
+    public String checkFile(){
+        boolean isValidExcel = true;
+        try {
+            File file = new File(this.exceldatei);
+
+            if (!file.exists() || file.isDirectory()) {
+                isValidExcel = false;
+            } else {
+                if (!(exceldatei.endsWith(".xls") || exceldatei.endsWith(".xlsx"))) {
+                    isValidExcel = false;
+                } else {
+                    try {
+                        WorkbookFactory.create(file);
+                    } catch (IOException e) {
+                        isValidExcel = false;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            isValidExcel = false;
+        }
+        if (!isValidExcel) {
+            return "File is not a valid Excel file: " + exceldatei;
+        }
+        else {
+            return "File is a valid Excel file" + exceldatei;
+        }
+    }
+
+    public void toCSV() {
+
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,12 @@ public class Main {
     public static void main(String[] args) {
         ExcelReader er = new ExcelReader("FSI-2023-DOWNLOAD.xlsx");
 
+        // Check File Methode
+        System.out.println(er.checkFile());
+
         // ExcelReaderMethode
         String text = er.ExcelReaderString();
         System.out.println(text);
-
 
     }
 }
